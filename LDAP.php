b2d1c2a03198fb678250a25e9185ca48f3bdfd23
Esteban serna palacios <108249063+OscurloOriginal@users.noreply.github.com>
2023-10-03 17:59:52 -0500

    Update LDAP.php
    cambie un poco la estructura para que los valores no fueran estáticos

--- LDAP.php
+++ LDAP.php
@@ -7,28 +7,39 @@ use InvalidArgumentException;
 
 /**
  * @see https://github.com/OscurloOriginal/
- * @version 1
+ * @version 1.1
  * @author Esteban serna palacios
  */
 
 class LDAP
 {
-    /**
-     * @param String $user
-     * @param String $pass
-     * @param String $search
-     * @param Array $attributes
-     * @param Array $setOptions
-     * @return Array
-     */
-    static function connect(String $user, String $pass, String $search = "", array $attributes = [], array $setOptions = []): array
+
+    public $uri, $dn, $base;
+    public $port = 389;
+
+    public function __construct()
     {
-        if (empty($user) || empty($pass))
-            throw new InvalidArgumentException("Usuario y contraseña son obligatorios");
+        $this->uri = getenv("USERDNSDOMAIN");
+        $this->dn = getenv("USERDOMAIN") . "\\";
+        $this->base = "DC=" . getenv("USERDOMAIN") . ",DC=COM";
+    }
+
+    /**
+     * @param String $user Usuario
+     * @param String $pass Contraseña
+     * @param String $search Valor a filtrar una vez establecida la conexión
+     * @param Array $attributes Campos del directorio activo por los cuales puede filtrar
+     * @param Array $setOptions Configuración de ldap enviar nombre de la constante y el valor para ldap_set_option
+     * @return Array Retorna los datos según las coincidecia del filtro enviado
+     */
+    public function connect(String $user, String $pass, String $search = "*", array $attributes = ["cn", "sn", "givenName", "mail"], array $setOptions = []): array
+    {
+        $this->dn = $this->dn . $user;
+        // if (empty($user) || empty($pass)) throw new InvalidArgumentException("Usuario y contraseña son obligatorios");
+        if (empty($pass)) throw new InvalidArgumentException("Usuario y contraseña son obligatorios");
 
         # format filter
-        $base = "dc=" . getenv("USERDOMAIN") . ",dc=COM";
-
+        $search = ldap_escape($search, "*", LDAP_ESCAPE_FILTER);
         $filter = "(|" . implode("", array_map(function ($x) use ($search) {
             return "({$x}={$search})";
         }, $attributes)) . ")";
@@ -42,15 +53,18 @@ class LDAP
 
         # connect ldap
         try {
-            $ldap = ldap_connect(getenv("USERDNSDOMAIN"));
+            $ldap = ldap_connect($this->uri, $this->port);
+
             foreach ($setOptions as $key => $value) if (defined($key))
                 ldap_set_option($ldap, constant($key), $value);
 
-            $ldap_bind = @ldap_bind($ldap, getenv("USERDOMAIN") . "\\{$user}", $pass);
-            if (!$ldap_bind) throw new Exception("Error al autenticar el usuario", ldap_error($ldap));
+            $ldap_bind = ldap_bind($ldap, $this->dn, $pass);
+            if (!$ldap_bind) throw new Exception("Error al autenticar el usuario: " . ldap_error($ldap));
 
-            # filter
-            $ldap_search = @ldap_search($ldap, $base, $filter, $attributes);
+            # search
+            $ldap_search = @ldap_search($ldap, $this->base, $filter, $attributes);
+            if (!$ldap_search) throw new Exception("Error al realizar la búsqueda LDAP: " . ldap_error($ldap));
+
             $ldap_get_entries = ldap_get_entries($ldap, $ldap_search);
         } catch (Exception $th) {
             throw new Exception("Error al conectar con LDAP: {$th->getMessage()}", $th->getCode());
